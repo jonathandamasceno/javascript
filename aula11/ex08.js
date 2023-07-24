d13c9f865444d68247029ad8aa83a40c3575e8fb
Jonathan Damasceno <139007515+jonathandamasceno@users.noreply.github.com>
2023-07-23 21:58:15 -0300

    Aula 11

---
+++ aula11/ex08.js
@@ -0,0 +1,8 @@
+var vel = 50
+console.log('A velocidade maxima permitida é 50km/h')
+
+if (vel > 50){//condição simples (se)
+    console.log('Você ultrapassou o limite permitido, MULTADO!!!')
+}else{ // (se não)
+    console.log('Continue assim :)')
+}
---
+++ aula11/ex09.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vive em ${pais}`)
+if (pais != 'Brasil'){
+    console.log('Você é estrangeiro')
+}else{
+    console.log('Você é Brasileiro')
+}
