42b783ea68f714cc8068e5a05b268c3286c5fb1f
Jonathan Damasceno <139007515+jonathandamasceno@users.noreply.github.com>
2023-07-26 15:45:07 -0300

    Exercício Novo

---
+++ exercícios/aula12ex/script.js
@@ -0,0 +1,22 @@
+function carregar(){
+    var msg = document.getElementById("msg")
+    var img = document.getElementById("imagem")
+    var agora = new Date()
+    var hora = agora.getHours()
+    //variaveis
+
+    msg.innerHTML = `<h2>Agora são ${hora} horas</h2>`
+
+
+    //mudança da foto e cor de fundo
+    if(hora >= 0 && hora < 12){
+        img.src = 'manha.png'
+        document.body.style.background = '#FFADA0'
+    }else if(hora >= 12 && hora <= 18){
+        img.src = 'tarde.png'
+        document.body.style.background = '#F6C18B'
+    }else{
+        img.src = 'noite.png'
+        document.body.style.background = '#0E2829'
+    }
+}
