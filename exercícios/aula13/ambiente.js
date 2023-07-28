b2ce5524afc4a0f3a4a41021421a245fcc3af36e
Jonathan Damasceno <139007515+jonathandamasceno@users.noreply.github.com>
2023-07-27 21:16:04 -0300

    Estrutura de controle

---
+++ exercícios/aula13/ambiente.js
@@ -0,0 +1,6 @@
+//inicio ; teste ; incremento
+console.log('inicio')
+for (var c = 1; c<=8 ; c++ ){
+    console.log(`Passo ${c}`)
+}
+console.log('final')
---
+++ exercícios/aula13/ambiente2.js
@@ -0,0 +1,14 @@
+//estrutura de repetição com teste no início 
+/*while( c <= 6 ){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+
+
+//estrutura de repetição com teste no final 
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 6)
+
