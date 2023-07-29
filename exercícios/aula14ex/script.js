4552dd46b456f622f109bad37ad56f291477da9f
Jonathan Damasceno <139007515+jonathandamasceno@users.noreply.github.com>
2023-07-29 15:06:32 -0300

    Continuação

--- exercícios/aula14ex/script.js
+++ exercícios/aula14ex/script.js
@@ -1,1 +1,27 @@
+function contar(){
+    var n1 = Number(document.getElementById('inicio').value)
+    var n2 = Number(document.getElementById('fim').value)
+    var numPasso = Number(document.getElementById('passo').value)
+    var res = document.getElementById('res')
+    let cont
+    
+    res.innerHTML = null
+    if(n1 < n2 ){
+        cont = n1
+        
+        while(cont <= n2){
+            res.innerHTML += `${cont} &#x1F449;`
+            cont ++
+        }
+    }else if (n1> n2){
+        cont = n1
+        while(cont >= n2){
+            res.innerHTML += `${cont} &#x1F449;`
+            cont --
+        }
 
+    }else{
+        res.innerHTML += `[ERRO] Coloque dados diferentes`
+    }
+
+}
