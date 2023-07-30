2b826b75340a61b7942a0e9e355f4df448f289c1
Jonathan Damasceno <139007515+jonathandamasceno@users.noreply.github.com>
2023-07-29 22:05:19 -0300

    Inicio do ex2

---
+++ exercícios/aula14ex/parte 2/script.js
@@ -0,0 +1,11 @@
+let txtnum = document.getElementById('numero')
+let res = document.getElementById('res')    
+
+function gerar(){
+
+    if(txtnum.value.length == 0 ){
+        res.innerHTML = ("Por favor digite um número!")
+          
+        }  
+    }
+
