e6831425f1600bf581d0c9d88f61ea1c2174b69e
Jonathan Damasceno <139007515+jonathandamasceno@users.noreply.github.com>
2023-08-03 12:54:58 -0300

    continuação

--- exercícios/aula15/script.js
+++ exercícios/aula15/script.js
@@ -1,15 +1,18 @@
 //variáveis compostas
 
 let num = [1 , 8 , 3 , 2 , 5]//coloca com virgulas entre chaves
-num[5] = 7
-num.push(6)//coloca o 6 na ultima posicao
-//num.length quantidade de números
-num.sort()//coloca em ordem
+//num[5] = 7
 
-/*for(let pos = 0; pos<num.lenght; pos++){
-    console.log(`O número é ${num} e a posição é ${pos}`)
+//num.push(6)coloca o 6 na ultima posicao
+
+//num.length quantidade de números
+
+//num.sort()coloca em ordem
+
+/*for(let pos = 0; pos < num.length; pos++){
+    console.log(`a posição é ${pos} e o numero é ${num[pos]}`)
 }*/
 
 for(let pos in num){
-    console.log(num[pos])
+    console.log(`a posição é ${pos} e o numero é ${num[pos]}`)
 }
