e91deafc0198d84a8eefb70887a909853e1d61ad
Jonathan Damasceno <139007515+jonathandamasceno@users.noreply.github.com>
2023-08-05 20:45:28 -0300

    Aula 16

---
+++ exercícios/aula16/funcao01.js
@@ -0,0 +1,9 @@
+function parimp (n){
+    if(n%2==0){
+        return 'Esse número é  Par'
+}   else{
+        return 'Esse número é Impar'
+}
+}
+let res = parimp(11)
+console.log(res)// ou console.log(parimp(11))
---
+++ exercícios/aula16/funcao03.js
@@ -0,0 +1,5 @@
+let m = function(x){
+    return x*2
+}
+console.log(m(7))
+//colocando função numa variavel
